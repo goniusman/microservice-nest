d31e2643c44e8c60b2e098cbea84de3d71092233
goniusman <goniusman400@gmail.com>
2026-06-09 12:12:54 +0600

    fixxed testing order

--- services/order/src/main.ts
+++ services/order/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { RabbitMQService } from './rabbitmq/rabbitmq.service';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
   const rabbit = app.get(RabbitMQService);
   await rabbit.connect();
   await app.listen(process.env.PORT ?? 3003);
+
 }
 bootstrap();
